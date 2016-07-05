6651eeaa50d9123ea88766fb06896760707de112
slugger70 <slugger70@gmail.com>
2016-07-05 10:00:39 -0500

    Added some files and modified the irida download links

---
+++ roles/irida/files/setenv.sh
@@ -0,0 +1,5 @@
+export CATALINA_OPTS="-Xms512m -Xmx1g \
+        -XX:+CMSClassUnloadingEnabled \
+        -XX:+CMSPermGenSweepingEnabled \
+        -XX:+UseConcMarkSweepGC \
+        -XX:MaxPermSize=256m"
